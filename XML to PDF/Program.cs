a096495b550b25b6adecc39a417f2763138850b0
Nicolas <Nicolas@DESKTOP-AHS4M28>
2023-05-22 13:37:01 -0300

    Agregar archivos de proyecto.

---
+++ XML to PDF/Program.cs
@@ -0,0 +1,21 @@
+﻿// Create a new PDF document
+using iText.Html2pdf;
+using iText.Kernel.Pdf;
+using System.Net.Http;
+
+string outputFilePath = "output.pdf";
+PdfWriter writer = new(outputFilePath);
+PdfDocument pdfDoc = new(writer);
+
+// Set up the pdfHTML converter
+ConverterProperties converterProperties = new();
+
+// Read HTML Content
+string htmlFilePath = "index.html";
+string htmlContent = File.ReadAllText(htmlFilePath);
+
+// Convert HTML to PDF
+HtmlConverter.ConvertToPdf(htmlContent, pdfDoc, converterProperties);
+
+
+pdfDoc.Close();
